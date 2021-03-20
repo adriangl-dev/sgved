e9edee1b6b5fdcf58faad7e46b9e5eb772b7482c
Virginia <larissa.yaiza@gmail.com>
2021-03-20 21:24:39 +0100

    Initial commit

---
+++ src/main/java/com/tfm/sgved/SgvedApplication.java
@@ -0,0 +1,13 @@
+package com.tfm.sgved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgvedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgvedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfm/sgved/SgvedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfm.sgved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
